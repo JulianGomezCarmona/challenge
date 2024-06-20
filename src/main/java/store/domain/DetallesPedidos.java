e574c97cc1885dcf25e1d24cb5852905c2011694
Julian Gomez <jgomez@dynamiasoluciones.com>
2024-06-20 15:38:32 -0500

    version 2

--- src/main/java/store/domain/DetallesPedidos.java
+++ src/main/java/store/domain/DetallesPedidos.java
@@ -1,4 +1,56 @@
 package store.domain;
 
-public class DetallesPedidos {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import tools.dynamia.domain.OrderBy;
+import tools.dynamia.domain.jpa.BaseEntity;
+
+@Entity
+@Table(name = "DetallesPedidos")
+@OrderBy("id")
+public class DetallesPedidos extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long pedidoId;
+    private int cantidad;
+    private int precio;
+
+
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
+
+
--- src/main/java/store/viewmodel/PedidoViewModel.java
+++ src/main/java/store/viewmodel/PedidoViewModel.java
@@ -2,12 +2,18 @@ package store.viewmodel;
 
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 import store.domain.Client;
+import store.domain.DetallesPedidos;
 import store.domain.Products;
 import tools.dynamia.domain.services.CrudService;
 import tools.dynamia.domain.util.DomainUtils;
+import tools.dynamia.zk.util.ZKBindingUtil;
+import tools.dynamia.zk.util.ZKUtil;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PedidoViewModel {
     private CrudService crudService = DomainUtils.lookupCrudService();
@@ -15,6 +21,9 @@ public class PedidoViewModel {
     private List<Client> clients;
     private Products selectdProduct;
     private List<Products> products;
+    private int Cantidad;
+    private List<DetallesPedidos> detallesPedidos = new ArrayList<>();
+
 
     @Init
     public void initPage() {
@@ -22,5 +31,70 @@ public class PedidoViewModel {
         products = crudService.findAll(Products.class);
         clients = crudService.findAll(Client.class);
     }
+//    @Command
+//    @NotifyChange("productPrice")
+
+//    public int priceProduct(){
+//
+//        System.out.println(priceProduct);
+//        return priceProduct;
+//    }
+
+    @Command
+    public void crearDetalle(){
+        int cantidad = getCantidad();
+        String nombreProducto = getSelectdProduct().getNombre();
+        String nombreCliente = getSelectedClient().getNombre();
+        int precio = getSelectdProduct().getPrecio();
+        DetallesPedidos  detallesPedido = new DetallesPedidos();
+        detallesPedido.setCantidad(cantidad);
+        detallesPedido.setPrecio(precio);
+        detallesPedidos.add(detallesPedido);
+        setSelectedClient(null);
+        setSelectdProduct(null);
+        setCantidad(0);
+        ZKBindingUtil.postNotifyChange(this);
+    }
+
+    public Client getSelectedClient() {
+        return selectedClient;
+    }
+
+    public void setSelectedClient(Client selectedClient) {
+        this.selectedClient = selectedClient;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public Products getSelectdProduct() {
+        return selectdProduct;
+    }
+
+    public void setSelectdProduct(Products selectdProduct) {
+        this.selectdProduct = selectdProduct;
+    }
+
+    public List<Products> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Products> products) {
+        this.products = products;
+    }
+
+    public int getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        Cantidad = cantidad;
+    }
+
 
 }
